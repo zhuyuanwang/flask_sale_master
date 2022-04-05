113a8233f5cd90016c1ac4bf817c9a46904b6445
zhuyuanwang <17746572170@163.com>
2022-04-05 23:11:15 +0800

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/4/5 12:42
+# @Author  : Circleone_
+
+from flask import Flask
+from flask_login import LoginManager
+
+def register_web_blueprint(app):
+    from app.web import web
+    app.register_blueprint(web)
+
+def create_app(config=None):
+    app = Flask(__name__)
+    # app.config.from_object('app.config')
+    register_web_blueprint(app)
+
+    return app
---
+++ app/config.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/4/5 12:59
+# @Author  : Circleone_
